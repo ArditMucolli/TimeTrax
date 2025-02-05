3f7affce837f8790f478e2e1a72a31430f7e1e1c
ArditMucolli <mucolliardit@gmail.com>
2025-02-05 14:03:29 +0100

    .

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
+import {View, StyleSheet} from 'react-native';
 import Footer from './components/Footer';
 import Homepage from './pages/Homepage';
 import ProfileScreen from './pages/ProfileScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Homepage"
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Homepage" component={Homepage} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-      <Footer />
+      <View style={styles.container}>
+        <Stack.Navigator
+          initialRouteName="Homepage"
+          screenOptions={{
+            headerShown: false,
+            cardStyle: {backgroundColor: '#F1F5FF'},
+          }}>
+          <Stack.Screen name="Homepage" component={Homepage} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+
+        <Footer />
+      </View>
     </NavigationContainer>
   );
 };
@@ -27,6 +32,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F1F5FF',
+  },
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: 60,
   },
 });
 
