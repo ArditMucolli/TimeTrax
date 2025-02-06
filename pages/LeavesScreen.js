09f32b9eba01dce07e55adf733cd57ca5413d1fa
ArditMucolli <mucolliardit@gmail.com>
2025-02-06 10:01:11 +0100

    feat: switch tab on leaves screen

--- pages/LeavesScreen.js
+++ pages/LeavesScreen.js
@@ -1,12 +1,98 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import Header from '../components/Header';
 
 const LeavesScreen = () => {
+  const [activeTab, setActiveTab] = useState('Leaves');
+
   return (
-    <View>
-      <Text>Leaves Screen</Text>
+    <View style={styles.container}>
+      <Header Title="Leaves" />
+      <View style={styles.switchContainer}>
+        <TouchableOpacity
+          style={[styles.tab, activeTab === 'Leaves' && styles.activeTab]}
+          onPress={() => setActiveTab('Leaves')}>
+          <Text
+            style={[
+              styles.tabText,
+              activeTab === 'Leaves' && styles.activeTabText,
+            ]}>
+            Leaves
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.tab, activeTab === 'Table' && styles.activeTab]}
+          onPress={() => setActiveTab('Table')}>
+          <Text
+            style={[
+              styles.tabText,
+              activeTab === 'Table' && styles.activeTabText,
+            ]}>
+            Table
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>
+        {activeTab === 'Leaves' ? (
+          <Text>Leaves View Content</Text>
+        ) : (
+          <Text>Table View Content</Text>
+        )}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1F5FF',
+    padding: 16,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    width: 330,
+    height: 45,
+    marginTop: 30,
+    borderRadius: 26,
+    paddingHorizontal: 10,
+    borderTopWidth: 3,
+    borderTopColor: '#D1D5DB',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 3},
+    shadowOpacity: 0.001,
+    shadowRadius: 6,
+    alignSelf: 'center',
+  },
+  tab: {
+    flex: 1,
+    height: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 5,
+    borderRadius: 26,
+  },
+  activeTab: {
+    backgroundColor: '#041F4E',
+  },
+  tabText: {
+    color: '#333',
+    fontSize: 16,
+    fontWeight: 600,
+  },
+  activeTabText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  content: {
+    marginTop: 30,
+    padding: 8,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+  },
+});
 
 export default LeavesScreen;
